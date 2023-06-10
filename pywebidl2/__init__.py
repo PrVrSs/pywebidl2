283e055b760cafe56016dde7a747ca40f44c5cea
Sergey Reshetnikov <resh.sersh@gmail.com>
2023-06-10 17:13:43 +0700

    Add isort

--- pywebidl2/__init__.py
+++ pywebidl2/__init__.py
@@ -2,6 +2,7 @@ from .idl import parse, raw_parse, validate
 from .parser import Parser as WebIDLParser
 from .visitor import Visitor as WebIDLVisitor
 
+
 __all__ = (
     'WebIDLParser',
     'WebIDLVisitor',
--- pywebidl2/utils.py
+++ pywebidl2/utils.py
@@ -1,6 +1,6 @@
 from collections import deque
 from contextlib import suppress
-from typing import Any, Optional, TypeVar, Union
+from typing import Any, TypeVar
 
 
 _U = TypeVar('_U')
@@ -17,7 +17,7 @@ def setup_type(idl_type: Any, type_: str) -> None:
                 todo.extend(idl_type.idl_type)
 
 
-def escaped_name(string: Optional[_U]) -> Union[Optional[_U], str]:
+def escaped_name(string: _U | str) -> _U | str:
     if isinstance(string, str):
         return string.lstrip('_')
 
